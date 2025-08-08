96a01993a2356f42f818672ba32020e2ee484b05
Dima Volkov <xli4nost@mail.ru>
2025-08-08 18:03:31 +0300

    colors and font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,11 @@
 import type {Metadata} from "next";
-import {Geist, Geist_Mono} from "next/font/google";
+import {Noto_Sans} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
-});
+const notoSans = Noto_Sans({
+    variable: "--font-noto-sans",
+    subsets: ["latin"]
+})
 
 export const metadata: Metadata = {
     title: "EasyPeak",
@@ -23,7 +18,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="ru">
-        <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <body className={notoSans.variable}>
         {children}
         </body>
         </html>
