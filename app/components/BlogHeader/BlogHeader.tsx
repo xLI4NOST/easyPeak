8ee304a11a81032def562080dfa3c901a4b94521
Dima Volkov <xli4nost@mail.ru>
2025-08-25 15:15:39 +0300

    fix marks

--- app/components/BlogHeader/BlogHeader.tsx
+++ app/components/BlogHeader/BlogHeader.tsx
@@ -1,18 +1,18 @@
+"use client"
+
 import styles from './BlogHeader.module.sass'
 import React from "react";
 import {GitHubIcon} from "@/app/components/BlogHeader/GitHubIcon";
 
 export const BlogHeader = (): React.ReactElement => {
+    const fun = () => {
+        console.log('hello')
+    }
+
 
     return <div className={styles.blogHeader}>
 
         <nav className={styles.blogHeaderNav}>
-            {/*<ul className={styles.headerNav_Burger}>*/}
-            {/*    <li>Новости</li>*/}
-            {/*    <li>О блоге</li>*/}
-            {/*    <li>Полезное</li>*/}
-            {/*</ul>*/}
-
             <ul>
                 <li>Новости</li>
                 <li>О блоге</li>
